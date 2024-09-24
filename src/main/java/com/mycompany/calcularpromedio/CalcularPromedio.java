a1bad060d2bac7c5615d0b73c76de22b9b243f35
Mario Alberto Campo Londoño <mariocampo.dev@gmail.com>
2024-09-23 23:34:21 -0500

    initial commit

---
+++ src/main/java/com/mycompany/calcularpromedio/CalcularPromedio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calcularpromedio;
+
+/**
+ *
+ * @author mario
+ */
+public class CalcularPromedio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
