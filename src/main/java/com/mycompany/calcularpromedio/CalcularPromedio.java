ceefcb20f66a2230a63e9b7bbb44323320dc77b2
Mario Alberto Campo Londoño <mariocampo.dev@gmail.com>
2024-09-23 23:36:22 -0500

    calculador promedio matriz

--- src/main/java/com/mycompany/calcularpromedio/CalcularPromedio.java
+++ src/main/java/com/mycompany/calcularpromedio/CalcularPromedio.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.calcularpromedio;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mario
@@ -11,6 +13,26 @@ package com.mycompany.calcularpromedio;
 public class CalcularPromedio {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        double notasMatriz[][] = new double [4][4], contadorPromedio = 0, totalPromedio = 0;
+        Scanner teclado = new Scanner(System.in);
+        
+        for (int i = 0; i < notasMatriz.length; i++) {
+            for (int j = 0; j < notasMatriz.length-1; j++) {
+                System.out.println("Ingrese la nota de la posicion: "+i);
+                notasMatriz[i][j] = teclado.nextDouble();
+                contadorPromedio = contadorPromedio + notasMatriz[i][j];
+            }
+            totalPromedio = contadorPromedio/(notasMatriz.length-1);
+            notasMatriz[i][notasMatriz.length-1] = totalPromedio;
+            contadorPromedio = 0;
+        }
+        
+        for (int i = 0; i < notasMatriz.length; i++) {
+            System.out.println("Las notas del alumno #"+i+" son:");
+            for (int j = 0; j < notasMatriz.length-1; j++) {
+                System.out.println("Nota #"+j+" con valor de: "+notasMatriz[i][j]);
+            }
+            System.out.println("El valor del promedio es de: "+notasMatriz[i][notasMatriz.length-1]);
+        }
     }
 }
